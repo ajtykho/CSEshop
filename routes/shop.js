09f29bcfd0a7204a9631e0bf45698b91832d8697
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-20 16:30:55 -0700

    module 2 progress

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,18 @@
 const path = require('path');
 
 const express = require('express');
-const { getProducts } = require('../controllers/products');
+const { getProducts } = require('../controllers/shop');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('checkout', shopController.getCheckout);
 
 module.exports = router;
