ebe58994fb46176dba4a92d51de5bdf56e356446
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-23 01:08:32 -0700

    stuck on error

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts =  (req, res, next) => {
     Product.fetchAll(products => {
@@ -8,6 +9,17 @@ exports.getProducts =  (req, res, next) => {
         path: '/products',
       });
           
+    });
+};
+
+exports.getProduct =  (req, res, next) => {
+    const prodId = req.params.id;
+    Product.findById(prodId, product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
     });
 };
 
@@ -23,14 +35,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('/shop/cart', {
+  res.render('shop/cart', {
     path: '/cart',
     pageTitle: "Your Cart"
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  })
+  res.redirect('/cart');
+};
+
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: "Your Orders"
+  });
+};
+
 exports.getCheckout = (req, res, next) => {
-  res.render('/shop/checkout', {
+  res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: "Checkout"
   });
---
+++ models/cart.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    //fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart.JSON.parse(fileContent);
+      }
+      //Analyze the cart => Find existing product
+      const existingproductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingproduct = cart.products[existingproductIndex];
+      let updatedProduct;
+      //add new product/increase quantity
+      if (existingproduct) {
+        updatedProduct = { ...existingproduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingproductIndex];
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+          console.log(err);
+      })
+    });
+  }
+};
