82b645f9722af530aed2f962e5691b3a1e17faec
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-23 22:21:05 -0700

    fixed problem

--- models/cart.js
+++ models/cart.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
-const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+const p =  path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
     //fetch the previous cart
+    console.log("please work:", p);
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
-        cart.JSON.parse(fileContent);
+        cart = JSON.parse(fileContent);
       }
       //Analyze the cart => Find existing product
       const existingproductIndex = cart.products.findIndex(
@@ -22,7 +23,7 @@ module.exports = class Cart {
         updatedProduct = { ...existingproduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
-        cart.products[existingproductIndex];
+        cart.products[existingproductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
