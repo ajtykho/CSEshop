e91bed49ea7d5f2fd2ef8a26cecf439565c9bf71
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-28 00:51:48 -0700

    big errors

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const express = require('express');
-const { getProducts } = require('../controllers/shop');
 
 const shopController = require('../controllers/shop');
 
@@ -11,14 +10,16 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:id', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-router.get('/orders', shopController.getOrders);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('checkout', shopController.getCheckout);
+router.post('/create-order', shopController.postOrder);
+
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
