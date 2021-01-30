21c9bfac73df1a7696271b9f288f76cc72e951b1
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-30 15:26:40 -0700

    04 complete

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require('path');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
-
-const errorController = require('./controllers/404');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -14,13 +16,67 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+ app.use((req, res, next) => {
+     User.findById('6015d903dbfe5a4c38f4ded7')
+      .then(user => {
+        req.user = user;
+         next();
+       })
+      .catch(err => console.log(err));
+  });
+
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/', shopRoutes);
 
-app.use(errorController.pageNotFound);
+app.use(errorController.get404);
 
+mongoose
+  .connect(
+    'mongodb+srv://ajtykho:Winkie24@cluster0.efjkr.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Ali',
+          email: 'ali@test.com',
+          cart: {
+            items: []
+          }
+        });
+        req.user = user;
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-app.listen(PORT); 
+//add-ins from prove04
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://class-shop-341.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://ajtykho:Winkie24@cluster0.efjkr.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+// mongoConnect((client) => {
+//     console.log("app is on local host");
+//     app.listen(3000);
+// });
